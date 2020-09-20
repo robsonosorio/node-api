1fe92af0d1d752f4ad91995e161780d47c03993a
Robson Osorio <robson.osorioo@gmail.com>
2020-09-20 19:13:57 -0300

    user - model

---
+++ src/app/models/User.js
@@ -0,0 +1,18 @@
+import Sequelize, { Model } from 'sequelize';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password_hash: Sequelize.STRING,
+        adm_user: Sequelize.BOOLEAN,
+        active: Sequelize.BOOLEAN,
+      },
+      {
+        sequelize,
+      }
+    );
+  }
+}
