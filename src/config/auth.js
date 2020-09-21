ef20303acd44ecb8ad8c7824c6332d7c4a349e05
Robson Osorio <robson.osorioo@gmail.com>
2020-09-21 06:51:40 -0300

    user finalizado

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '881c45bbce3b9970b83b5c6fb36d88d0',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,10 @@
 import { Router } from 'express';
+import SessionController from './app/controllers/SessionController';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth'
 
 const routes = new Router();
 
@@ -8,7 +12,12 @@ routes.get('/', (res, req) => {
   return req.json({ Bem: 'vindo' });
 });
 
+routes.get('/users', UserController.index);
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
 routes.put('/users', UserController.update);
 
 export default routes;
